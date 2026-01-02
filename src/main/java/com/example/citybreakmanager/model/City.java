57166ceb760a0f583e5af1643bb1820dad99242b
SKharel10 <sagarkharel001@gmail.com>
2026-01-02 22:46:45 +0000

    correctly updated relationship between trip and model

--- src/main/java/com/example/citybreakmanager/model/City.java
+++ src/main/java/com/example/citybreakmanager/model/City.java
@@ -1,9 +1,21 @@
 package com.example.citybreakmanager.model;
 
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
+@Entity
 public class City {
+    @Id
+    @Column(name="city_id")
     private int id;
     private String name;
     private String country;
     private String details;
+
+    @OneToMany(mappedBy = "trip_id")
+    private List<Trip> listOfTrips;
+
 }
 
--- src/main/java/com/example/citybreakmanager/model/Trip.java
+++ src/main/java/com/example/citybreakmanager/model/Trip.java
@@ -1,8 +1,17 @@
 package com.example.citybreakmanager.model;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
 public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="trip_id")
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
     private City city;
     private LocalDate startDate;
     private LocalDate endDate;
